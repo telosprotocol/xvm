da9accb20eb3d680ada76d1bd6f479d6ac57bbe9
payton.wu <payton.wu@topnetwork.org>
2021-12-22 11:45:58 +0800

    fix(xvm): fix rebase compiling error.

--- xcontract_helper.cpp
+++ xcontract_helper.cpp
@@ -2,8 +2,10 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "xchain_fork/xchain_upgrade_center.h"
 #include "xcontract_helper.h"
+
+#include "xbasic/xerror/xthrow_error.h"
+#include "xchain_fork/xchain_upgrade_center.h"
 #include "xstore/xstore_error.h"
 #include "xvm/xerror/xvm_error.h"
 
